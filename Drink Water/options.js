d73a26d96c1bfb2745f10238c4202c832adc4470
Bhavya Soni <rkumarbhavy@gmail.com>
2025-06-01 01:02:09 +0530

    Added function to save settings

--- Drink Water/options.js
+++ Drink Water/options.js
@@ -3,4 +3,14 @@ document.addEventListener('DOMContentLoaded', () => {
         document.getElementById('interval').value = data.interval || 60
         document.getElementById('mode').value = data.mode || 'video'
     })
+
+    document.getElementById("save").addEventListener("click", () => {
+        const interval = document.getElementById('interval').value
+        const mode = document.getElementById('mode').value
+
+        chrome.storage.sync.set({ interval, mode }, () => {
+            chrome.runtime.sendMessage({ action: "updateAlarm", interval: parseInt(interval) });
+            document.getElementById('status').innerText = '✅ Settings saved!';
+        })
+    })
 })
